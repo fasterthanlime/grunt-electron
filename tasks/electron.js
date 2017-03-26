d7670f497123755bfa0ce00b4f7bcc7e730dbf29
Amos Wenger <amos@itch.io>
2017-03-26 14:18:45 +0200

    rename, depend on electron-rebuild

--- tasks/electron.js
+++ tasks/electron.js
@@ -1,11 +1,26 @@
 'use strict';
 const electronPackager = require('electron-packager');
+const rebuild = require('electron-rebuild');
 
 module.exports = grunt => {
 	grunt.registerMultiTask('electron', 'Package Electron apps', function () {
 		const done = this.async();
 
-		electronPackager(this.options(), err => {
+		const options = Object.assign({}, this.options(), {
+			afterCopy: [
+				(buildPath, electronVersion, platform, arch, callback) => {
+					grunt.log('Rebuilding native dependencies');
+					rebuild(buildPath, electronVersion, arch, [], true)
+						.then(() => done())
+						.catch((e) => {
+							grunt.warn('Building modules didn\'t work!');
+							grunt.fatal(e);
+						});
+				}
+			]
+		});
+
+		electronPackager(options, err => {
 			if (err) {
 				grunt.warn(err);
 				done();
