554b8b2e81c703e4757b45eedbb50f8a4ceed358
Amos Wenger <amos@itch.io>
2017-07-15 14:33:31 +0200

    Work around https://github.com/npm/npm/issues/17781

--- tasks/electron.js
+++ tasks/electron.js
@@ -1,6 +1,8 @@
 'use strict';
 const electronPackager = require('electron-packager');
 const rebuild = require('electron-rebuild').default;
+const path = require('path');
+const rimraf = require('rimraf');
 
 module.exports = grunt => {
 	grunt.registerMultiTask('electron', 'Package Electron apps', function () {
@@ -11,7 +13,12 @@ module.exports = grunt => {
 				(buildPath, electronVersion, platform, arch, callback) => {
 					grunt.log.writeln('Rebuilding native dependencies');
 					rebuild(buildPath, electronVersion, arch, [], true)
-						.then(() => callback())
+						.then(() => function () {
+							const binPath = path.join(buildPath, 'node_modules', '.bin');
+							grunt.log.writeln('Wiping ' + binPath + ' to make npm@5.3.0 happy');
+							rimraf.sync(binPath);
+							callback();
+						})
 						.catch((e) => {
 							grunt.warn('Building modules didn\'t work!');
 							grunt.fatal(e);
